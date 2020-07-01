b1b540b3a7113010aa3975193251c5ad1670c7c4
André Góis <andre.gois@tradiio.com>
2020-07-01 14:21:04 +0100

    interface containers

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,6 @@ const synth = new Synth().toMaster();
 
 function App() {
 
-  let x = 0;
-  const y = 0;
-
-
   //See annotations in JS for more information
   const setup = (p5: p5Types | any, canvasParentRef: Element) => {
     p5.createCanvas(p5.windowWidth -20, p5.windowHeight -20).parent(canvasParentRef);
@@ -28,7 +24,14 @@ function App() {
   return (
     <div className="App">
 
-      <img src="/logo512.png" className="logo" onClick={onPress}/>
+      <img src="/logo512.png" className="logo" onClick={onPress} alt="wv"/>
+
+      <div className="interface-container">
+        <div className="">
+          <p>hello</p>
+          <button className="btn" onClick={onPress}>play</button>
+        </div>
+      </div>
 
       <div className="sketch-container">
         <Sketch setup={setup} draw={draw} />
