1810d39ba6e0bff717151d2631f3c8fce21d89d5
André Góis <andre.gois@tradiio.com>
2020-07-01 16:03:17 +0100

    added oscilloscope

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,35 @@ import React from 'react';
 import './App.css';
 import Sketch from 'react-p5';
 import p5Types from 'p5';
-import { Synth } from "tone";
+import { Synth, FFT, Waveform } from "tone";
 
 const synth = new Synth().toMaster();
+const analyser = new Waveform(512);
+synth.chain(analyser);
 
 function App() {
 
   //See annotations in JS for more information
   const setup = (p5: p5Types | any, canvasParentRef: Element) => {
     p5.createCanvas(p5.windowWidth -20, p5.windowHeight -20).parent(canvasParentRef);
+    p5.stroke(255);
   };
 
   const draw = (p5: p5Types | any) => {
     p5.background(10);
+    let spectrum = analyser.getValue();
+      for (let i = 0; i < spectrum.length; i++) {
+        p5.point(
+          p5.map(i, 0, spectrum.length, 0, p5.windowWidth - 20),
+          p5.map(spectrum[i], -1, 1, p5.windowHeight -20 / 2, 0)
+        );
+      }
   };
 
   const onPress = () => {
-    synth.triggerAttackRelease("A3", "8n");
+    const octave = Math.round(Math.random() * 5 + 1);
+    synth.triggerAttackRelease(`A${octave}`, "8n");
+    // console.log(analyser.getValue());
   }
 
   return (
