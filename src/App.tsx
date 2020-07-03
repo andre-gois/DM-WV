1a7931850d4edead14aad4d92a43cb6a2399d94f
André Góis <andre.gois@tradiio.com>
2020-07-03 17:51:25 +0100

    added tone loop

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import './App.scss';
 import Sketch from 'react-p5';
 import p5Types from 'p5';
-import { Waveform, MembraneSynth, Freeverb } from "tone";
+import { Waveform, MembraneSynth, Freeverb, Loop, Transport } from "tone";
 import Slider from './components/Slider/Slider';
 import Dice from './components/Dice/Dice';
 import Sequencer from './components/Sequencer/Sequencer';
 
-console.log('STARTUP');
+// console.log('STARTUP');
 
 const TEMPO = 120;
 const ATTACK = 0.001;
@@ -16,12 +16,12 @@ const SUSTAIN = 0.01;
 const RELEASE = 1.4;
 const PITCH_DECAY = 0.05;
 const REVERB_DAMPENING = 40;
-const REVERB_WET = 0.3;
+const REVERB_WET = 0.9;
 
 const soundParamsInit = [
-  { id: 'tempo', min: 20, max: 220, step: 1, name: 'T', value: TEMPO },
+  // { id: 'tempo', min: 20, max: 220, step: 1, name: 'T', value: TEMPO },
   { id: 'attack', min: 0.001, max: 1.0, step: 0.001, name: 'A', value: ATTACK },
-  { id: 'decay', min: 0.01, max: 2.0, step: 0.01, name: 'D', value: DECAY },
+  { id: 'decay', min: 0.01, max: 1.0, step: 0.01, name: 'D', value: DECAY },
   { id: 'release', min: 0.1, max: 4, step: 0.1, name: 'R', value: RELEASE },
   { id: 'pitch_decay', min: 0.0, max: 0.5, step: 0.01, name: '\\', value: PITCH_DECAY },
   { id: 'reverb_dampening', min: 20, max: 150, step: 10, name: 'V', value: REVERB_DAMPENING },
@@ -56,6 +56,11 @@ synth.connect(freeverb);
 synth.chain(analyser);
 synth.volume.value = -10;
 
+const songSequence = (time) => {
+  const octave = Math.round(Math.random() * 3 + 1);
+  synth.triggerAttackRelease(`A${octave}`, `4n`, time);
+}
+const loop = new Loop(songSequence, '4n');
 
 const convertSoundParamsToSynthOptions = (param, val) => {
   switch (param.id) {
@@ -86,7 +91,7 @@ function App() {
 
   //See annotations in JS for more information
   const setup = (p5: p5Types | any, canvasParentRef: Element) => {
-    console.log('setup:')
+    // console.log('setup:')
     p5.createCanvas(p5.windowWidth -20, p5.windowHeight -20).parent(canvasParentRef);
     p5.stroke(100);
     p5.frameRate(50);
@@ -107,13 +112,16 @@ function App() {
   };
 
   const onPress = () => {
-    console.log('onPress:');
-    const octave = Math.round(Math.random() * 3 + 1);
-    synth.triggerAttackRelease(`A${octave}`, `4n`);
+    Transport.start();
+    if (loop.state === 'started') {
+      loop.stop();
+    } else {
+      loop.start(0);
+    }
   }
 
   const changeSoundParam = (index, val) => {
-    console.log('changeSoundParam:', index, val)
+    // console.log('changeSoundParam:', index, val)
     setSoundParams(prev => {
       prev[index].value = val;
       convertSoundParamsToSynthOptions(prev[index], val);
@@ -122,7 +130,7 @@ function App() {
   }
 
   const radomizeAllSoundParams = () => {
-    console.log('radomizeAllSoundParams:');
+    // console.log('radomizeAllSoundParams:');
     soundParams.forEach((param, i) => {
       const val = Math.random() * (param.max - param.min) + param.min;
       changeSoundParam(i, val);
@@ -136,7 +144,7 @@ function App() {
       <img src="/logo512.png" className="logo" alt="wv"/>
 
       <div className="interface-container">
-        <div className="sequencer">
+        <div className="sequencer-container">
           <Sequencer />
         </div>
         <div className="center-area">
--- src/components/Sequencer/Sequencer.tsx
+++ src/components/Sequencer/Sequencer.tsx
@@ -1,6 +1,6 @@
 
 
-import React, { useState } from "react";
+import React from "react";
 import './Sequencer.scss';
 import Dice from "../Dice/Dice";
 
@@ -10,12 +10,38 @@ interface SequencerComponentProps {
 
 const Sequencer = ({ onPress }: SequencerComponentProps) => {
 
+    // const [bas, setBa] = useState(false);
+
     return (
         <div className="sequencer">
-                <div className="tracks">T</div>
-                <div className="randomize">
-                    <Dice onPress={() => null}/>
+            <div className="tracks">
+                <div className="track">
+                    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(v=> (
+                        <div key={v} className="step"></div>
+                    ))}
                 </div>
+                <div className="track">
+                    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(v => (
+                        <div key={v} className="step">
+                            <div className="active"></div>
+                        </div>
+                    ))}
+                </div>
+                <div className="track">
+                    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(v => (
+                        <div key={v} className="step"></div>
+                    ))}
+                </div>
+                <div className="track">
+                    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(v => (
+                        <div key={v} className="step"></div>
+                    ))}
+                </div>
+            </div>
+            <div className="randomize">
+                <Dice onPress={() => null}/>
+                <div className="line"></div>
+            </div>
         </div>
     )
 }
