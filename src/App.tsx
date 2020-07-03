7f5b5dec99844345e414e9ed180bc8e4182a9724
André Góis <andre.gois@tradiio.com>
2020-07-04 00:17:31 +0100

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Sketch from 'react-p5';
 import p5Types from 'p5';
-import { Waveform, MembraneSynth, Freeverb, Loop, Transport } from "tone";
+import { Waveform, MembraneSynth, Freeverb, Loop, Transport, Distortion } from "tone";
 import Slider from './components/Slider/Slider';
 import Dice from './components/Dice/Dice';
 import Sequencer from './components/Sequencer/Sequencer';
 
 // console.log('STARTUP');
 
-const TEMPO = 120;
+const TEMPO = 166;
 const ATTACK = 0.001;
 const DECAY = 0.4;
 const SUSTAIN = 0.01;
@@ -17,15 +17,15 @@ const RELEASE = 1.4;
 const PITCH_DECAY = 0.05;
 const REVERB_DAMPENING = 40;
 const REVERB_WET = 0.9;
+const DISTORTION = 0.4;
 
 const soundParamsInit = [
-  // { id: 'tempo', min: 20, max: 220, step: 1, name: 'T', value: TEMPO },
-  { id: 'attack', min: 0.001, max: 1.0, step: 0.001, name: 'A', value: ATTACK },
+  { id: 'attack', min: 0.001, max: 0.07, step: 0.001, name: 'A', value: ATTACK },
   { id: 'decay', min: 0.01, max: 1.0, step: 0.01, name: 'D', value: DECAY },
-  { id: 'release', min: 0.1, max: 4, step: 0.1, name: 'R', value: RELEASE },
   { id: 'pitch_decay', min: 0.0, max: 0.5, step: 0.01, name: '\\', value: PITCH_DECAY },
   { id: 'reverb_dampening', min: 20, max: 150, step: 10, name: 'V', value: REVERB_DAMPENING },
   { id: 'reverb_wet', min: 0, max: 1, step: 0.1, name: 'W', value: REVERB_WET },
+  { id: 'distortion', min: 0.0, max: 1, step: 0.05, name: 'X', value: DISTORTION },
 ];
 
 const membraneOptions = {
@@ -50,11 +50,19 @@ const voice3 = new MembraneSynth(membraneOptions).toMaster();
 const voice4 = new MembraneSynth(membraneOptions).toMaster();
 const analyser = new Waveform(512);
 
+Transport.bpm.value = TEMPO;
+
+const distortion = new Distortion(DISTORTION).toMaster();
+
 const freeverb = new Freeverb().toMaster();
 freeverb.dampening.value = REVERB_DAMPENING;
 freeverb.roomSize.value = 0.9;
 freeverb.wet.value = REVERB_WET;
 
+voice1.connect(distortion);
+voice2.connect(distortion);
+voice3.connect(distortion);
+voice4.connect(distortion);
 voice1.connect(freeverb);
 voice2.connect(freeverb);
 voice3.connect(freeverb);
@@ -69,11 +77,12 @@ voice3.volume.value = -10;
 voice4.volume.value = -10;
 
 const tracksPreset = [
-  [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-  [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
-  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-  [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0]
+  [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
 ];
+
 
 const convertSoundParamsToSynthOptions = (param, val) => {
   switch (param.id) {
@@ -89,11 +98,8 @@ const convertSoundParamsToSynthOptions = (param, val) => {
       voice3.envelope.decay = val;
       voice4.envelope.decay = val;
       break;
-    case 'release':
-      voice1.envelope.release = val;
-      voice2.envelope.release = val;
-      voice3.envelope.release = val;
-      voice4.envelope.release = val;
+    case 'distortion':
+      distortion.distortion = val;
       break;
     case 'reverb_dampening':
       freeverb.dampening.value = val;
@@ -110,6 +116,12 @@ const convertSoundParamsToSynthOptions = (param, val) => {
   }
 };
 
+function randomWithProbability() {
+  var notRandomNumbers = [0, 0, 0, 0, 1];
+  var idx = Math.floor(Math.random() * notRandomNumbers.length);
+  return notRandomNumbers[idx];
+}
+
 let stepTone = 0;
 let tracksTone= [...tracksPreset];
 
@@ -117,11 +129,13 @@ function App() {
 
   const [soundParams, setSoundParams] = useState(soundParamsInit);
   const [loop, setLoop] = useState();
+  const [playing, setPlaying] = useState(false);
+  const [tempo, setTempo] = useState(TEMPO);
   const [tracks, setTracks] = useState(tracksPreset);
   const [activeStep, setActiveStep] = useState(-1);
 
   useEffect(() => {
-    setLoop(new Loop(songSequence, '4n'));
+    setLoop(new Loop(songSequence, '8n'));
   }, []);
 
   useEffect(() => {
@@ -131,16 +145,16 @@ function App() {
 
   const songSequence = (time) => {
     if (tracksTone[0][stepTone]) {
-      voice1.triggerAttackRelease(`C3`, `4n`, time);
+      voice1.triggerAttackRelease(`C1`, `4n`, time);
     }
     if (tracksTone[1][stepTone]) {
-      voice2.triggerAttackRelease(`D3`, `4n`, time);
+      voice2.triggerAttackRelease(`Eb2`, `4n`, time);
     }
     if (tracksTone[2][stepTone]) {
-      voice3.triggerAttackRelease(`E3`, `4n`, time);
+      voice3.triggerAttackRelease(`G2`, `4n`, time);
     }
     if (tracksTone[3][stepTone]) {
-      voice4.triggerAttackRelease(`F4`, `4n`, time);
+      voice4.triggerAttackRelease(`C3`, `4n`, time);
     }
     setActiveStep(prev => (prev + 1) % 16);
   }
@@ -170,8 +184,10 @@ function App() {
     Transport.start();
     if (loop.state === 'started') {
       loop.stop();
+      setPlaying(false);
     } else {
       setActiveStep(-1);
+      setPlaying(true);
       loop.start(0);
     }
   }
@@ -193,13 +209,18 @@ function App() {
 
   const randomizeSequencer = () => {
     const newState = [...tracks];
-    setTracks(newState.map(track => track.map(step => Math.round(Math.random()))))
+    setTracks(newState.map(track => track.map(step => randomWithProbability())))
   }
 
   const changeStep = (trackIndex, stepIndex) => {
     const newState = [...tracks];
     newState[trackIndex][stepIndex] = Math.abs(newState[trackIndex][stepIndex] - 1);
     setTracks(newState);
+  }
+
+  const changeTempo = (tempo) => {
+    Transport.bpm.value = tempo;
+    setTempo(tempo);
   }
 
 
@@ -210,10 +231,10 @@ function App() {
 
       <div className="interface-container">
         <div className="sequencer-container">
-          <Sequencer activeStep={activeStep} tracks={tracks} onPress={(trackIndex, stepIndex) => changeStep(trackIndex, stepIndex)} randomizeSequencer={randomizeSequencer}/>
+          <Sequencer changeTempo={val => changeTempo(val)} tempo={tempo} activeStep={activeStep} tracks={tracks} onPress={(trackIndex, stepIndex) => changeStep(trackIndex, stepIndex)} randomizeSequencer={randomizeSequencer}/>
         </div>
         <div className="center-area">
-          <img src="/play_sm.png" onClick={onPress} className="play" alt="play"/>
+          <img src={playing ? '/stop_sm.png' : '/play_sm.png'} onClick={onPress} className="play" alt="play"/>
         </div>
         <div className="controls">
           <div className="params">
@@ -233,7 +254,7 @@ function App() {
         <Sketch setup={setup} draw={draw} />
       </div>
 
-      <footer>v1.0</footer>
+      <footer>DM/WV v1.0</footer>
     </div>
   );
 }
--- src/components/Sequencer/Sequencer.tsx
+++ src/components/Sequencer/Sequencer.tsx
@@ -3,33 +3,39 @@
 import React from "react";
 import './Sequencer.scss';
 import Dice from "../Dice/Dice";
+import Slider from "../Slider/Slider";
 
 interface SequencerComponentProps {
     activeStep: number;
     tracks: [[number]];
     onPress: any;
     randomizeSequencer: any;
+    tempo: number;
+    changeTempo: any;
 }
 
-const Sequencer = ({ onPress, activeStep, tracks, randomizeSequencer }: SequencerComponentProps) => {
+const Sequencer = ({ onPress, activeStep, tracks, randomizeSequencer, tempo, changeTempo }: SequencerComponentProps) => {
 
     return (
-        <div className="sequencer">
-            <div className="tracks">
-                {tracks.map((track, trackIndex) => (
-                    <div key={trackIndex} className="track">
-                        {track.map((step, stepIndex) => (
-                            <div key={stepIndex} className={`step clickable ${stepIndex === activeStep ? 'active' : ''}`} onClick={() => onPress(trackIndex, stepIndex)}>
-                                {step === 1 ? <div className="active"/> : null}
-                            </div>
-                        ))}
-                    </div>
-                ))}
-            </div> {activeStep}
-            <div className="randomize">
-                <Dice onPress={() => randomizeSequencer()}/>
-                <div className="line"></div>
+        <div className="seq-tempo">
+            <div className="sequencer">
+                <div className="tracks">
+                    {tracks.map((track, trackIndex) => (
+                        <div key={trackIndex} className="track">
+                            {track.map((step, stepIndex) => (
+                                <div key={stepIndex} className={`step clickable ${stepIndex === activeStep ? 'active' : ''}`} onClick={() => onPress(trackIndex, stepIndex)}>
+                                    {step === 1 ? <div className="active"/> : null}
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+                <div className="randomize">
+                    <Dice onPress={() => randomizeSequencer()}/>
+                    <div className="line"></div>
+                </div>
             </div>
+            <Slider name="" value={tempo} step={1} min={20} max={300} onChange={val => changeTempo(val)} />
         </div>
     )
 }
